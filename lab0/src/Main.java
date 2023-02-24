a5152de3084db5fdae0f4cf1a7642ea2162956f8
gutudaniil17 <101668867+gutudaniil17@users.noreply.github.com>
2023-02-24 09:22:34 +0200

    Lab0

---
+++ lab0/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        File fileInput = new File(".\\src\\MOCK_DATA.csv");
+        String stringInput = "Chickie;Hoggan;Madagascar;1989;47;IM/nChickie;Hoggan;Madagascar;1989;47;IM";
+        File fileOutput = new File(".\\src\\MOCK_DATA2.csv");
+
+
+        ArrayList<Competitor> array = Competitor.input(fileInput);
+        ArrayList<Competitor> array2 = Competitor.input(stringInput);
+        /*for(Competitor competitor:array){
+            System.out.println(competitor);
+        }
+
+        for(Competitor competitor: array2){
+            System.out.println(competitor);
+        }*/
+        Competitor.output(array,fileOutput);
+        Competitor.output(array2);
+
+    }
+}
